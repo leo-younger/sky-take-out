9edd7001f6d7634a6052ee7f8afa0fec10e4b0e0
leo-younger <2547134259@qq.com>
2026-08-11 17:21:42 +0800

    添加购物车的业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,38 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * @author Young
+ */
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    /**
+     * 根据条件查询购物车
+     * @param cart 购物车对象
+     * @return 购物车列表
+     */
+    List<ShoppingCart> list(ShoppingCart cart);
+
+    /**
+     * 根据id更新数量
+     * @param shoppingCart 购物车对象
+     */
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 添加购物车
+     * @param cart 购物车对象
+     */
+    @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
+            "values (#{name}, #{image}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime});")
+    void add(ShoppingCart cart);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,77 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import com.sky.vo.SetmealVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Young
+ */
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    /**
+     * 添加购物车
+     *
+     * @param shoppingCartDTO 购物车数据
+     */
+    @Override
+    public void add(ShoppingCartDTO shoppingCartDTO)
+        {
+        log.info("添加购物车：{}", shoppingCartDTO);
+        //先判断购物车中是否已经存在该菜品
+        ShoppingCart cart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, cart);
+        cart.setUserId(BaseContext.getCurrentId());
+        List<ShoppingCart> list = shoppingCartMapper.list(cart);
+
+        //如果不存在则添加购物车
+        if (list == null || list.isEmpty()) {
+            //判断本次添加的是菜品还是套餐
+            Long dishId = shoppingCartDTO.getDishId();
+            if (dishId != null) {
+                //本次添加的是菜品
+                Dish dish = dishMapper.getById(dishId);
+                cart.setName(dish.getName());
+                cart.setImage(dish.getImage());
+                cart.setAmount(dish.getPrice());
+            } else {
+                SetmealVO setmeal = setmealMapper.selectByPrimaryKey(shoppingCartDTO.getSetmealId());
+                cart.setName(setmeal.getName());
+                cart.setImage(setmeal.getImage());
+                cart.setAmount(setmeal.getPrice());
+            }
+            cart.setNumber(1);
+            cart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.add(cart);
+        } else {
+            //如果存在则更新数量
+            ShoppingCart shoppingCart = list.get(0);
+            shoppingCart.setNumber(shoppingCart.getNumber() + 1);
+            shoppingCartMapper.updateNumberById(shoppingCart);
+        }
+
+        }
+}
